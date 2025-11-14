159d3981e30924d4b4a2f1d7c53425359804e88c
gauravkanwal <kanwalgk25@gmail.com>
2025-11-14 07:54:27 +0530

    initial commit

---
+++ models/expense.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose');
+
+const expenseSchema=mongoose.Schema({
+    userID:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
+
+    title:String,
+    ammount:Number,
+    category:String,
+    paymentType:String,
+    date:{
+        type:Date,
+        default:Date.now
+    },
+    createdAt,
+    updatedAt
+})
+
+module.exports=mongoose.model('expense',expenseSchema);
